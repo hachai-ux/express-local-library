49f0325d33473978472822ac1f4f5042ffa23c61
Hac Hai Pham <hpham.ux@gmail.com>
2022-01-19 04:58:19 +0100

    Setup express framework

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'You are so cool' });
+});
+
+module.exports = router;
