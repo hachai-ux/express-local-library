dea6dfb90302e451ca1b6a6142f2324fb60dc1c9
Hac Hai Pham <hpham.ux@gmail.com>
2022-01-24 13:32:34 +0100

    Create catalog route module

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/catalog');
 });
 
-router.get('/cool', function(req, res, next) {
-  res.render('index', { title: 'You are so cool' });
-});
+
 
 module.exports = router;
