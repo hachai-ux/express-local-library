b748af887c7de64a28ab711c889a1462703a7f6c
Hac Hai Pham <hpham.ux@gmail.com>
2022-01-27 12:10:32 +0100

    Add get function for book instance delete page

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -95,7 +95,19 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_delete', { title: 'Delete Book Instance ', bookinstance: bookinstance});
+    })
 };
 
 // Handle BookInstance delete on POST.
