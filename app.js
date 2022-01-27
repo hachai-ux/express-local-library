e26364a7c1364b910cc97cafc5aee70bcbdf6325
Hac Hai Pham <hpham.ux@gmail.com>
2022-01-27 14:19:31 +0100

    Fix mongodb server link

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://user:password123456@cluster0.qurhq.mongodb.net/local_library?retryWrites=true&w=majoritys';
+var mongoDB = 'mongodb+srv://user:password123456@cluster0.qurhq.mongodb.net/local_library?retryWrites=true';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
